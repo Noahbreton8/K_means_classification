01d409aa64033ca33f69e3776f6052797856b2c4
noahbreton8 <noahbreton8@gmail.com>
2024-11-06 14:04:02 -0500

    add all k means clustering

--- A3codes.py
+++ A3codes.py
@@ -7,6 +7,8 @@ import os
 import scipy.special._logsumexp as log
 import pandas as pd
 import A3helpers
+import random
+import scipy.spatial.distance as d
 
 solvers.options['show_progress'] = False
 #1
@@ -54,8 +56,8 @@ def synRegExperiment():
 #a
 def PCA(X, k):
     n, d = X.shape
-    U = np.mean(X, axis=0)
-    U = X - U
+    mu = np.mean(X, axis=0)
+    U = X - mu
     res = linalg.eigh(U.T @ U)
     return res[1][:, -k:].T
 
@@ -65,8 +67,117 @@ def fashion_MNIST():
     U = PCA(dataset, 20)
     A3helpers.plotImgs(U)
     
-fashion_MNIST()
+# fashion_MNIST()
+
 #c 
 def projPCA(Xtest, mu, U):
-    Xproj = Xtest - mu.T
+    Xproj = (Xtest - mu.T)@U.T
     return Xproj
+
+#d
+def synClsExperimentsPCA():
+    n_runs = 100
+    n_train = 128
+    n_test = 1000
+    dim_list = [1, 2]
+    gen_model_list = [1, 2]
+    train_acc = np.zeros([len(dim_list), len(gen_model_list), n_runs])
+    test_acc = np.zeros([len(dim_list), len(gen_model_list), n_runs])
+    for r in range(n_runs):
+        for i, k in enumerate(dim_list):
+            for j, gen_model in enumerate(gen_model_list):
+                Xtrain, Ytrain = A3helpers.generateData(n=n_train, gen_model=gen_model)
+                Xtest, Ytest = A3helpers.generateData(n=n_test, gen_model=gen_model)
+                U = PCA(Xtrain, k)
+                mu = np.mean(Xtrain, axis=0)
+                Xtrain_proj = projPCA(Xtrain, mu, U) # TODO: call your projPCA to find the new features
+                Xtest_proj = projPCA(Xtest, mu, U) # TODO: call your projPCA to find the new features
+                Xtrain_proj = A3helpers.augmentX(Xtrain_proj) # add augmentation
+                Xtest_proj = A3helpers.augmentX(Xtest_proj)
+                W = minMulDev(Xtrain_proj, Ytrain) # from Q1
+                Yhat = classify(Xtrain_proj, W) # from Q1
+                train_acc[i, j, r] = calculateAcc(Yhat, Ytrain) # from Q1
+                Yhat = classify(Xtest_proj, W)
+                test_acc[i, j, r] = calculateAcc(Yhat, Ytest)
+    
+    train_accuracy = np.mean(train_acc, axis=2)
+    test_accuracy = np.mean(test_acc, axis=2)
+
+    return train_accuracy, test_accuracy
+    # TODO: compute the average accuracies over runs
+    # TODO: return 2-by-2 train accuracy and 2-by-2 test accuracy
+
+# print(synClsExperimentsPCA())
+#e
+
+
+#3
+#a
+
+def kmeans(X, k, max_iter=1000):
+    n, d = X.shape
+    assert max_iter > 0
+
+    indices= np.random.choice(n, k, replace=False)  
+    U = X[indices]                                       # TODO: Choose k random points from X as initial centers
+    for i in range(max_iter):
+        D = pairwise_distance(X, U)             # TODO: Compute pairwise distance between X and U
+        Y = cluster_assignments(D)              # TODO: Find the new cluster assignments
+        old_U = U
+        U = np.linalg.inv((Y.T @ Y + 1e-8 * np.eye(k)))@ Y.T @ X   # TODO: Update cluster centers
+        if np.allclose(old_U, U):
+            break
+    W = X - Y@U
+    obj_val = 1/(2*n) * np.trace(W.T @ W) **2                 # TODO: Compute objective value
+    return Y, U, obj_val
+
+def pairwise_distance(X, U):
+    # D = []
+    # for row in X:
+    #     D_row = []
+    #     for row2 in U:
+    #         D_row.append(np.sum(row - row2))
+    #     D.append(D_row)
+    # return D
+    return d.cdist(X, U)
+
+#b
+def cluster_assignments(D):
+    max_indices = np.argmin(D, axis=1)
+
+    Y = np.zeros_like(D)
+    for index, max_index in enumerate(max_indices):
+        Y[index][max_index] = 1
+    return Y
+
+
+#b
+def repeatKmeans(X, k, n_runs=100):
+    best_obj_val = float('inf')
+    best_y = None
+    best_u = None
+    for r in range(n_runs):
+        Y, U, obj_val = kmeans(X, k)
+        # TODO: Compare obj_val with best_obj_val. If it is lower,
+        # then record the current Y, U and update best_obj_val
+        if obj_val < best_obj_val:
+            best_obj_val = obj_val
+            best_y = Y
+            best_u = U
+    return best_y, best_u, best_obj_val
+    # TODO: Return the best Y, U and best_obj_val
+
+#c
+def chooseK(X, k_candidates=[2,3,4,5,6,7,8,9]):
+    obj_val_list = []
+    for k in k_candidates:
+        _, _, obj_val = repeatKmeans(X,k)
+        obj_val_list.append(obj_val)
+
+    return obj_val_list
+
+Xtrain, Ytrain = A3helpers.generateData(n=100, gen_model=2)
+obj_val_list = chooseK(Xtrain)
+print(obj_val_list)
+
+#[1219.365953773579, 402.0883975029475, 155.90767163304153, 104.82182371648109, 69.31305342824578, 51.60135632031073, 39.764469345950374, 31.42019298010047]
